25f2eb90a8370645c838ad0af288d2f3e43c3fc2
mlangTse <48012544+mlangTse@users.noreply.github.com>
2020-10-20 05:47:30 +1100

    Add files via upload

--- ass/client.py
+++ ass/client.py
@@ -2,15 +2,108 @@
 import sys
 import os
 from socket import *
-from json import dumps
+from json import dumps, loads
 
 HOST = gethostbyname(gethostname())
 PORT = 12345
 
-def sendMessage(clientSocket, Mtype, Mname, Message):
+class CommHandle():
+    def __init__(self, clientSocket):
+        self.clientSocket = clientSocket
+        self.recvData = None
+        self.executeComm = {
+            'CRT': self.CRT,
+            'MSG': self.MSG,
+            'DLT': self.DLT,
+            'EDT': self.EDT,
+            'LST': self.LST,
+            'RDT': self.RDT,
+            'UPD': self.UPD,
+            'DWN': self.DWN,
+            'RMV': self.RMV,
+            'XIT': self.XIT,
+            'SHT': self.SHT,
+        }
+
+    def recv(self):
+        self.recvData = self.clientSocket.recv(2048).decode('utf-8')
+
+    def CRT(self, args):
+        if len(args) != 1:
+            print('Invalid command')
+            return
+
+        sendMessage(self.clientSocket, 'CRT', 'name', args[0])
+        self.recv()
+        print(self.recvData)
+
+    def MSG(self, args):
+        if len(args) < 2:
+            print('Invalid command')
+            return
+
+        message = ' '.join(args[1:])
+        sendMessage(self.clientSocket, 'MSG', 'thread', args[0], 'message', message)
+        self.recv()
+        print(self.recvData)
+
+    def DLT(self, args):
+        sendMessage(self.clientSocket, 'DLT')
+        self.recv()
+        print(self.recvData)
+
+    def EDT(self, args):
+        sendMessage(self.clientSocket, 'EDT')
+        self.recv()
+        print(self.recvData)
+
+    def LST(self, args):
+        sendMessage(self.clientSocket, 'LST')
+        self.recv()
+        self.recvData = loads(self.recvData)
+        if not self.recvData:
+            print('No threads to list')
+            return
+
+        print('The list of active threads:')
+        for i in self.recvData:
+            print(i)
+
+    def RDT(self, args):
+        sendMessage(self.clientSocket, 'RDT')
+        self.recv()
+        print(self.recvData)
+
+    def UPD(self, args):
+        sendMessage(self.clientSocket, 'UPD')
+        self.recv()
+        print(self.recvData)
+
+    def DWN(self, args):
+        sendMessage(self.clientSocket, 'DWN')
+        self.recv()
+        print(self.recvData)
+
+    def RMV(self, args):
+        sendMessage(self.clientSocket, 'RMV')
+        self.recv()
+        print(self.recvData)
+
+    def XIT(self, args):
+        sendMessage(self.clientSocket, 'XIT')
+        self.recv()
+        print(self.recvData)
+
+    def SHT(self, args):
+        sendMessage(self.clientSocket, 'SHT')
+        self.recv()
+        print(self.recvData)
+
+def sendMessage(clientSocket, Mtype, Mname=None, Message=None, Mname2=None, Message2=None):
     message = {
         'type': Mtype,
         f'{Mname}': Message,
+        f'{Mname2}': Message2,
     }
     clientSocket.send(bytes(dumps(message), encoding='utf-8'))
 
@@ -20,7 +113,6 @@ def login(clientSocket):
 
     while True:
         recvData = clientSocket.recv(1024).decode('utf-8')
-        print(recvData)
         if recvData[:1] == 'Y':
             if len(recvData) == 2:
                 sendMessage(clientSocket, 'login-S', 'status', 'successfully logged in')
@@ -59,12 +151,17 @@ def client():
     commandList = ['CRT', 'MSG', 'DLT', 'EDT', 'LST', 'RDT', 'UPD', 'DWN', 'RMV', 'XIT', 'SHT']
 
     # get command from the user
-    while True:
+    Handler = CommHandle(clientSocket)
+    while Handler.recvData != 'Goodbye':
         command = input(f'Enter one of the following commands: '+' '.join(commandList))
 
-        if command == 'q':
-            sendMessage(clientSocket, 'q', 'q', 'q')
-            break
+        command = command.split(' ')
+
+        commandType = command[0]
+        if commandType not in Handler.executeComm.keys():
+            print('Invalid command')
+            continue
+        Handler.executeComm[commandType](command[1:])
 
     clientSocket.close()
 
--- ass/server.py
+++ ass/server.py
@@ -5,47 +5,135 @@ from socket import *
 from _thread import *
 import time
 import datetime as dt
-import json
+from json import dumps, loads
 
 # Store clients info
 clients = []
+thread = []
 
 class Client():
     def __init__(self, username, pwd):
         self.username = username
         self.pwd = pwd
+        self.conn = None
+        self.request = None
         self.logged = False
+        self.executeComm = {
+            'CRT': self.CRT,
+            'MSG': self.MSG,
+            'DLT': self.DLT,
+            'EDT': self.EDT,
+            'LST': self.LST,
+            'RDT': self.RDT,
+            'UPD': self.UPD,
+            'DWN': self.DWN,
+            'RMV': self.RMV,
+            'XIT': self.XIT,
+            'SHT': self.SHT,
+        }
 
-def signIn(conn, request):
+    def CRT(self):
+        global thread
+        if os.path.exists(self.request['name']):
+            message = 'Thread ' + self.request['name'] + ' exists'
+            print(message)
+            self.conn.send(bytes(message, encoding='utf-8'))
+            return
+
+        with open(self.request['name'], 'w') as f:
+            f.write(self.username)
+            f.close
+        thread.append(self.request['name'])
+        message = 'Thread ' + self.request['name'] + ' created'
+        self.conn.send(bytes(message, encoding='utf-8'))
+
+    def MSG(self):
+        if not os.path.exists(self.request['thread']):
+            message = 'Thread ' + self.request['thread'] + ' not exists'
+            print(message)
+            self.conn.send(bytes(message, encoding='utf-8'))
+            return
+
+        def file_len(fname):
+            with open(fname) as f:
+                for i, l in enumerate(f):
+                    continue
+            return str(i + 1)
+
+        with open(self.request['thread'], 'a') as f:
+            l = file_len(self.request['thread'])
+            f.write('\n' + l + ' ' + self.username + ': ' + self.request['message'])
+            f.close
+
+        message = 'Message posted to ' + self.request['thread'] + ' thread'
+        print(message)
+        self.conn.send(bytes(message, encoding='utf-8'))
+
+    def DLT(self):
+        pass
+
+    def EDT(self):
+        pass
+
+    def LST(self):
+        global thread
+        print(f'{self.username} issued LST command')
+        self.conn.send(bytes(dumps(thread), encoding='utf-8'))
+
+    def RDT(self):
+        pass
+
+    def UPD(self):
+        pass
+
+    def DWN(self):
+        pass
+
+    def RMV(self):
+        pass
+
+    def XIT(self):
+        self.logged = False
+        print(f'{self.username} exited')
+        self.conn.send(bytes('Goodbye', encoding='utf-8'))
+
+    def SHT(self):
+        pass
+
+def signIn(conn, request, client):
     if request['type'] == 'login-N':
         client = [i for i in clients if i.username == request['username']]
 
         # A new client
         if not client:
             client = Client(request['username'], None)
+            client.conn = conn
             clients.append(client)
             conn.send(bytes('NU' + request['username'], encoding='utf-8'))
-            return
+            return client
 
         client = client[0]
+        client.conn = conn
         if not client.logged:
             conn.send(bytes('PWD', encoding='utf-8'))
         else:
             conn.send(bytes('LD', encoding='utf-8'))
     elif request['type'] == 'login-P':
         if client.pwd == request['password']:
+            client.logged = True
             conn.send(bytes('Y', encoding='utf-8'))
         else:
             print('Incorrect password')
             conn.send(bytes('N', encoding='utf-8'))
     elif request['type'] == 'login-NP':
-        if not client.pwd:
-            client.pwd = request['password']
-            with open('credentials.txt', 'a') as f:
-                f.write(f'{client.username} {client.pwd}\n')
-            conn.send(bytes('YN', encoding='utf-8'))
+        client.pwd = request['password']
+        with open('credentials.txt', 'a') as f:
+            f.write(f'{client.username} {client.pwd}\n')
+        conn.send(bytes('YN', encoding='utf-8'))
     elif request['type'] == 'login-S':
         print(client.username, request['status'])
+
+    return client
 
 def updateClients():
     global clients
@@ -58,26 +146,24 @@ def updateClients():
 
 def ClientThread(conn):
     global clients
-    clients = updateClients()
     client = None
 
     while True:
         data = conn.recv(1024)
         print(f'received: {data}')
         if not data:
-            print('client disconnected')
+            print('Waiting for clients')
             break
 
-        request = json.loads(data.decode('utf-8'))
+        request = loads(data.decode('utf-8'))
 
-        if request['type'] == 'q':
-            client.logged = False
-            break
-
-        if request['type'].startwith('login'):
-            signIn(conn, request)
-
-
+        if request['type'].startswith('login'):
+            client = signIn(conn, request, client)
+        elif client:
+            if request['type'] != 'XIT':
+                print(f'{client.username} issued ', request['type'], ' command')
+            client.request = request
+            client.executeComm[request['type']]()
 
 def web_server(port):
     # Get host
@@ -95,7 +181,7 @@ def web_server(port):
 
     while True:
         # Wait for a connection
-        print('Waiting for a connection from TCP clients')
+        # print('Waiting for a connection from TCP clients')
         # print('Waiting for clients')
         conn, (ip, port) = TCPserver.accept()
 
@@ -118,4 +204,5 @@ if __name__ == "__main__":
 
     Admpass = sys.argv[2]
 
+    clients = updateClients()
     web_server(port)
